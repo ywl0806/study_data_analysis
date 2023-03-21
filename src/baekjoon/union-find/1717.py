b1e3ad2742ea48576d82fa02fc805ab239cc9b8c
이용우 <lee-yongwoo@yongwooM1MacbookAir.local>
2023-03-21 22:14:25 +0900

    commit

--- src/baekjoon/union-find/1717.py
+++ src/baekjoon/union-find/1717.py
@@ -1,8 +1,30 @@
-N, M = map(int, input().split())
+import sys
 
-root = list(range(N + 1))
 
-for n in range(M):
-    read, x, y = map(int, input().split())
+def find(x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
 
-# if(read == 1):
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x != y:
+        parent[y] = x
+
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+
+for _ in range(m):
+    op, a, b = map(int, input().split())
+    if op == 0:
+        union(a, b)
+    else:
+        if find(a) == find(b):
+            print("YES")
+        else:
+            print("NO")
